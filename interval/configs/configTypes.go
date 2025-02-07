72602ce2659ec18f149680f1a180d6fa328ba592
Trung Bùi <bui23@purdue.edu>
2025-02-06 21:16:11 -0500

    Delete interval directory

--- interval/configs/configTypes.go
+++
@@ -1,6 +0,0 @@
-package configs
-
-type MuopDBConfig struct {
-	Host string `yaml:"host"`
-	Port int    `yaml:"port"`
-}
--- interval/http/handler.go
+++
@@ -1,9 +0,0 @@
-package http
-
-import (
-	http2 "net/http"
-)
-
-func (app App) Insert(w http2.ResponseWriter, r *http2.Request) {
-
-}
